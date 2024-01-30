37f95776038f62c7a3dda647cde65547cdac2076
tuumama <ronybilletespc@gmail.com>
2024-01-29 22:05:24 -0600

    toda la pagina new

--- files/script.js
+++ files/script.js
@@ -1,4 +1,5 @@
 
+
 var typed = new Typed(".text", {
     strings: ["Mr Misael","Brandon", "Ronald", "Cristian", "Yarim", "Luis A", "Antonio", "Yael", "Alfred", "Alexa", "Jorge", "Carlos", "Angel", "Rebe", "Dariana"],
     typeSpeed: 100,
@@ -6,6 +7,21 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
 
 
 const toTop = document.querySelector(".top");
@@ -17,3 +33,5 @@ window.addEventListener("scroll", () => {
         toTop.classList.remove("active");
     }
 })
+
+
